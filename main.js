cdb7618fa21d2027cb04f39f30da5f9295c2a24b
Atoti2 <bogyikormi3@gmail.com>
2024-03-13 08:41:15 +0100

    random number generated

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import { check } from "./scripts/check.js";
+import { generateRandomNr } from "./scripts/generateRandomNr.js";
 
 document.querySelector("#check").addEventListener("click", check);
+
+let randomNumber 
+
+window.addEventListener('load', ()=> {
+    randomNumber = generateRandomNr(1,10)
+})
